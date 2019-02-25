6c842079c0bc6bc0a1d6c135f4647f1c17eea397
Felix Eder <felix.eder@gmx.com>
2019-02-25 15:41:27 +0100

    Feat #3 Add MainActivityUITest class

---
+++ app/src/androidTest/java/fr/free/nrw/commons/contributions/MainActivityUITest.java
@@ -0,0 +1,82 @@
+package fr.free.nrw.commons.contributions;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityUITest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onSaveInstanceState() {
+    }
+
+    @Test
+    public void onAuthCookieAcquired() {
+    }
+
+    @Test
+    public void setNumOfUploads() {
+    }
+
+    @Test
+    public void hideTabs() {
+    }
+
+    @Test
+    public void showTabs() {
+    }
+
+    @Test
+    public void onAuthFailure() {
+    }
+
+    @Test
+    public void onBackPressed() {
+    }
+
+    @Test
+    public void onBackStackChanged() {
+    }
+
+    @Test
+    public void onCreateOptionsMenu() {
+    }
+
+    @Test
+    public void onOptionsItemSelected() {
+    }
+
+    @Test
+    public void onActivityResult() {
+    }
+
+    @Test
+    public void onRequestPermissionsResult() {
+    }
+
+    @Test
+    public void onResume() {
+    }
+
+    @Test
+    public void onDestroy() {
+    }
+}
